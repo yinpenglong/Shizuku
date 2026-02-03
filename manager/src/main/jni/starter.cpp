b7ae05e9385b520af4e1fc764aa82be535187370
yinpenglong <abc20010207@qq.com>
2026-02-03 22:06:14 +0800

    Update starter.cpp
    实验加空格补位15找差异

--- manager/src/main/jni/starter.cpp
+++ manager/src/main/jni/starter.cpp
@@ -207,7 +207,7 @@ int main(int argc, char *argv[]) {
     }
 
     uid_t uid = getuid();
-    if (uid != 0 && uid != 2000 && uid != 1000) {
+    if (uid != 0 && uid != 2000               ) {
         perrorf("fatal: run Shizuku from non root nor adb nor system user (uid=%d).\n", uid);
         exit(EXIT_FATAL_UID);
     }
